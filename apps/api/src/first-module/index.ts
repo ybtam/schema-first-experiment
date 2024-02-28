aae6008fd48f78664f0fff53b71c9decfefaa363
Yi Tam <tam.yi3d@gmail.com>
2024-02-28 11:11:00 +0100

    move schema to gql file

--- apps/api/src/first-module/index.ts
+++ apps/api/src/first-module/index.ts
@@ -1,18 +1,16 @@
-import { createModule, gql } from 'graphql-modules'
+import { createModule } from 'graphql-modules'
+import  * as schema from './typedef/hello.graphql'
+import {FirstModuleModule} from "./typedef/module-types.ts";
+
+const resolvers: FirstModuleModule.Resolvers = {
+  Query: {
+    hello: () => 'world'
+  }
+}
 
 export const firstModule = createModule({
   id: 'first-module',
   dirname: __dirname,
-  typeDefs: [
-    gql`
-      type Query {
-        hello: String!
-      }
-    `
-  ],
-  resolvers: {
-    Query: {
-      hello: () => 'world'
-    }
-  }
+  typeDefs: [schema],
+  resolvers
 })
