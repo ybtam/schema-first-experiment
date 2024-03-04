47d6ff1981128e452b5662bfc1be17848ee6d9da
ybtam <47603803+ybtam@users.noreply.github.com>
2024-03-04 16:56:07 +0100

    7 improve db select performance (#8)
    * setting up schemas and updating queries to use db query method
    
    * feat: make queries more performant by only selecting selected columns

--- apps/api/src/user/resolvers/query.ts
+++ apps/api/src/user/resolvers/query.ts
@@ -2,20 +2,34 @@ import {UserModule} from "../typedef/module-types.ts";
 import {db} from "../../../db";
 import {users} from "../schema.ts";
 import {eq} from "drizzle-orm";
+import type {User} from "../../../generated-types/graphql.ts";
+import {getRequestedFields} from "../../../utils/get-request-fields.ts";
 
 const queries: UserModule.QueryResolvers = {
-  user: async (parent, {id}) => {
-    const selectedUsers = await db
-      .select()
-      .from(users)
-      .where(eq(users.id, id))
+  user: async (parent, {id}, context, info) => {
+    const requestedFields = getRequestedFields(info);
 
-    return selectedUsers[0]
+    return (
+      await db.query
+      .users
+      .findFirst({
+        columns: requestedFields,
+        where: eq(users.id, id)
+      })
+    ) as User;
   },
-  users: async (parent, ) => {
-    return db
-      .select()
-      .from(users);
+  users: async (parent, args, context, info ) => {
+    try {
+      const requestedFields = getRequestedFields(info);
+
+      return (await db.query.users.findMany({
+        columns: requestedFields
+      })) as User[];
+    } catch (e: unknown) {
+      console.error(e);
+
+      return []
+    }
   }
 }
 
